cf68fda6f3aefcf5d9e299ae0e556773cf859a51
Clefos <cristianclefos@mail.com>
2023-03-06 09:08:48 +0000

    grpc web testing with envoy proxy

--- binstat-ui/src/app/app.component.ts
+++ binstat-ui/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EchoServiceClient} from "../proto/binstat.pbsc";
+import {EchoRequest} from "../proto/binstat.pb";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'binstat';
+export class AppComponent implements OnInit {
+
+  inputText = '';
+
+  readonly responses: Array<string> = [];
+
+  constructor(
+    private echoServiceClient: EchoServiceClient
+  ) {
+  }
+
+  public ngOnInit() {
+  }
+
+  clicked() {
+    const req = new EchoRequest();
+    req.message = this.inputText;
+    this.inputText = '';
+    this.echoServiceClient.echo(req)
+      .subscribe(resp => this.responses.push(resp.message));
+  }
+
 }
--- binstat-ui/src/app/app.module.ts
+++ binstat-ui/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GrpcCoreModule } from "@ngx-grpc/core";
+import { GrpcWebClientModule } from "@ngx-grpc/grpc-web-client";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    GrpcCoreModule.forRoot(),
+    GrpcWebClientModule.forRoot({
+      settings: { host: 'http://0.0.0.0:9090' },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
