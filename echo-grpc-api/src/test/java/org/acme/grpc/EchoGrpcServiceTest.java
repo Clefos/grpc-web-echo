47c076b10e7999fb879970451b1b452ce82ea6e7
Clefos <cristianclefos@mail.com>
2025-04-10 21:23:33 +0100

    add integration test for streaming echo

--- echo-grpc-api/src/test/java/org/acme/grpc/EchoGrpcServiceTest.java
+++ echo-grpc-api/src/test/java/org/acme/grpc/EchoGrpcServiceTest.java
@@ -3,10 +3,12 @@ package org.acme.grpc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
+import java.util.List;
 
 import io.quarkus.grpc.GrpcClient;
 import io.quarkus.test.junit.QuarkusTest;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
@@ -17,7 +19,6 @@ class EchoGrpcServiceTest {
 
     @Test
     void testEcho() {
-
         EchoRequest echoRequest = EchoRequest.newBuilder()
                 .setMessage("Hello")
                 .build();
@@ -27,4 +28,20 @@ class EchoGrpcServiceTest {
         assertEquals(echoRequest.getMessage(), echoResponse.getMessage());
     }
 
+    @Test
+    void testStreamingEcho() {
+        EchoStreamingRequest echoRequest = EchoStreamingRequest.newBuilder()
+                .setMessage("Hello")
+                .setFrequency(1)
+                .setEchoes(2)
+                .build();
+        List<String> echoResponses = echoGrpc.echoStreaming(echoRequest)
+                .map(EchoResponse::getMessage)
+                .collect()
+                .asList()
+                .await()
+                .indefinitely();
+        List<String> expected = List.of("Hello", "Hello");
+        Assertions.assertIterableEquals(expected, echoResponses);
+    }
 }
